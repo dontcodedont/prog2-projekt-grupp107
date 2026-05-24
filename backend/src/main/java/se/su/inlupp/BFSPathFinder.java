146d7d7faf3b7bdbcfd3d466c72c183a74a70a29
TheBlubb <clara.elgebrandt@hotmail.com>
2026-05-24 12:10:40 +0200

    Test nr 6

--- backend/src/main/java/se/su/inlupp/BFSPathFinder.java
+++ backend/src/main/java/se/su/inlupp/BFSPathFinder.java
@@ -5,6 +5,7 @@ public class BFSPathFinder<T> implements PathFinder<T> {
   @Override
   public Path<T> findPath(Graph<T> graph, T from, T to) {
      //Test  jhasbjnanaj
+    //SAAÖL;ÖLSA;AÖL;As
     throw new UnsupportedOperationException("Unimplemented method 'findPath'");
   }
 }
