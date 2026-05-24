b159142dcff22e6a8fe6b6a50bb0aec9ef0c049c
dontcodedont <felixafgeijersstam@gmail.com>
2026-05-24 12:20:27 +0200

    felix

--- backend/src/main/java/se/su/inlupp/BFSPathFinder.java
+++ backend/src/main/java/se/su/inlupp/BFSPathFinder.java
@@ -6,6 +6,7 @@ public class BFSPathFinder<T> implements PathFinder<T> {
   public Path<T> findPath(Graph<T> graph, T from, T to) {
      //Test  jhasbjnanaj
     //SAAÖL;ÖLSA;AÖL;As
+    // felix komentar
     throw new UnsupportedOperationException("Unimplemented method 'findPath'");
   }
 }
